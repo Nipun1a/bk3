06e17992055d4c8c47019c91e526f6b0d5787b45
nipun1a <nipun1a1b@gmail.com>
2025-07-29 21:11:49 +0530

    dynamic routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -11,6 +12,15 @@ app.get("/", function(req , res) {
    // res.send("Hello World"); now we will render the pagies which are in views folder with the help of ejs
    res.render("index", {title: "Home Page"});
 });
+
+app.get("/profile/:username", function(req, res){
+    res.send(`Welcome, ${req.params.username}`);
+});
+
+app.get("/profile/:username/:age", function(req, res){
+    res.send(`Welcome, ${req.params.username}. You are ${req.params.age} years old.`);
+});
+
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
